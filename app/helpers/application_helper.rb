0ec1db03bdeab0f895e0f43c1ae0b9f9b73e7248
mpucholblasco <mpucholblasco@gmail.com>
2015-01-10 21:24:50 +0100

    Adding StyleSheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def nav_links
+    items = [disks_link, files_link]
+    content_tag :nav do
+      items.collect { |item| concat item}
+    end
+  end
+
+  def disks_link
+    nav_item_active_if current_page?(disks_path), 'Disks', controller: 'disks'
+  end
+
+  def files_link
+    nav_item_active_if current_page?(files_path), 'Files', controller: 'file'
+  end
+
+  def nav_item_active_if(condition, name, attributes)
+    if condition
+      a_class = "active"
+    end
+    link_to name, attributes, :class => a_class 
+  end
 end

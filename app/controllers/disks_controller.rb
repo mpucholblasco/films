0364462b6e7bebc0b38a7f284c32bdd2b3854e65
mpucholblasco <mpucholblasco@gmail.com>
2015-01-11 21:42:35 +0100

    Modified external disk location

--- app/controllers/disks_controller.rb
+++ app/controllers/disks_controller.rb
@@ -1,5 +1,5 @@
 class DisksController < ApplicationController
-  DISK_MOUNT_PATH = 'asdf'
+  DISK_MOUNT_PATH = '/media/usb/'
   def index
     @disks = Disk.paginate(:page => params[:page], per_page: 50).order(:name)
   end

d62675953105086f46d5a72c3d3e2f537d41e654
mpucholblasco <mpucholblasco@gmail.com>
2015-01-18 19:44:06 +0100

    Converted search get form into TurboLinks form and focus on load

---
+++ app/assets/javascripts/files.js
@@ -0,0 +1,15 @@
+$(document).on("ajax:beforeSend", "form[data-turboform]", function(e) {
+    Turbolinks.visit(this.action+(this.action.indexOf('?') == -1 ? '?' : '&')+$(this).serialize());
+    return false;
+});
+
+$(document).on('page:load', function(e) {
+	var AutofocusInput = $('input[autofocus="autofocus"]');
+	var value = AutofocusInput.val();
+	AutofocusInput.focus();
+	if (value !== undefined) {
+		var strLength= AutofocusInput.val().length * 2;
+		AutofocusInput[0].setSelectionRange(strLength, strLength);
+	}
+});
+

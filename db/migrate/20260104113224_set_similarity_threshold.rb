4a59d476903f5ff47ba5519aff75efa04208bb46
Marcel Puchol <mpucholblasco@gmail.com>
2026-01-04 12:38:37 +0100

    remove similarity threshold

--- db/migrate/20260104113224_set_similarity_threshold.rb
+++
@@ -1,9 +0,0 @@
-class SetSimilarityThreshold < ActiveRecord::Migration[8.0]
-  def up
-    execute "ALTER DATABASE #{connection.current_database} SET pg_trgm.similarity_threshold = 0.7"
-  end
-
-  def down
-    execute "ALTER DATABASE #{connection.current_database} SET pg_trgm.similarity_threshold = 0.3"
-  end
-end
